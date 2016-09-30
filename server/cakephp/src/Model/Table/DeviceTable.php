1f45f7a59460c2244664d30956e8796fd1e95809
shimomo0502 <github@shimomo.net>
2016-09-30 18:29:59 +0900

    [add] devices

---
+++ server/cakephp/src/Model/Table/DeviceTable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+/**
+ * @author shimo
+ */
+class DeviceTable extends Table
+{
+    /**
+     * 使用するテーブルの設定
+     *
+     * @param  array $config
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        $this->table('devices');
+        $this->primaryKey('id');
+    }
+}
