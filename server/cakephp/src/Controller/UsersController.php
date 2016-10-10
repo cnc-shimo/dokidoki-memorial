16ee41f0b8881636e20b08d87dc2bdad0c168f77
shimomo0502 <github@shimomo.net>
2016-10-10 13:58:10 +0900

    [update] change to call the model

--- server/cakephp/src/Controller/UsersController.php
+++ server/cakephp/src/Controller/UsersController.php
@@ -32,37 +32,7 @@ class UsersController extends AppController
      */
     public function index()
     {
-        $users = $this->Users->find()->join([
-            'c' => [
-                'table' => 'couples',
-                'type' => 'INNER',
-                'conditions' => [
-                    'OR' => [
-                        'c.man_id' => new \Cake\Database\Expression\IdentifierExpression('Users.id'),
-                        'c.woman_id' => new \Cake\Database\Expression\IdentifierExpression('Users.id'),
-                    ],
-                ],
-            ],
-        ])->select([
-            'id'                 => 'Users.id',
-            'name'               => 'Users.name',
-            'sex'                => 'Users.sex',
-            'birthday'           => 'Users.birthday',
-            'constellation'      => 'Users.constellation',
-            'blood'              => 'Users.blood',
-            'birthplace'         => 'Users.birthplace',
-            'residence'          => 'Users.residence',
-            'hobby'              => 'Users.hobby',
-            'created_at'         => 'Users.created_at',
-            'updated_at'         => 'Users.updated_at',
-            'couple_id'          => 'c.id',
-            'couple_man_id'      => 'c.man_id',
-            'couple_woman_id'    => 'c.woman_id',
-            'couple_anniversary' => 'c.anniversary',
-            'couple_created_at'  => 'c.created_at',
-            'couple_updated_at'  => 'c.updated_at',
-        ])->all();
-
+        $users = $this->Users->getUsers();
         $this->set([
             'users' => $users,
             '_serialize' => [
