fbe94c2a187548562210d049a5b1fe00f5e4dc87
kyoumoto <q677224k@mail.kyutech.jp>
2016-11-12 15:57:07 +0900

    [add] HowToCommunicateFragment.java

---
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/HowToCommunicateFragment.java
@@ -0,0 +1,29 @@
+package com.example.conami.dokidoki_memorial;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by matsushita on 2016/11/12.
+ */
+
+public class HowToCommunicateFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("タイトル")
+                .setMessage("メッセージ")
+                .create();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        // onPause でダイアログを閉じる場合
+        dismiss();
+    }
+}
