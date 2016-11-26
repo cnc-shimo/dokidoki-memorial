98f1ff10d2d2758f38ba92b99ae6c1886c5e6529
kyoumoto <q677224k@mail.kyutech.jp>
2016-11-27 05:29:11 +0900

    [update] User.java, FrustrationListFragment.java

--- client/app/src/main/java/com/example/conami/dokidoki_memorial/FrustrationListFragment.java
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/FrustrationListFragment.java
@@ -54,6 +54,7 @@ public class FrustrationListFragment extends Fragment implements SwipeRefreshLay
     View rootView;
     FrustrationMessageAdapter adapter;
     ArrayList<FrustrationMessage> messages;
+    User user = new User();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -108,7 +109,9 @@ public class FrustrationListFragment extends Fragment implements SwipeRefreshLay
             adapter.notifyDataSetChanged();
             }
         });
-        httpRequest.get("http://210.140.69.130/api/v1/users/1/frustrations.json");
+
+        String url = "http://210.140.69.130/api/v1/users/"+user.getId()+"/frustrations.json";
+        httpRequest.get(url);
         //httpRequest.execute("GET","http://www.google.co.jp");
         //textView.setText("aaaa");
 
--- client/app/src/main/java/com/example/conami/dokidoki_memorial/User.java
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/User.java
@@ -5,4 +5,32 @@ package com.example.conami.dokidoki_memorial;
  */
 
 public class User {
+    int id = 1;
+    String name = "user1";
+    int coupleId = 1;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCoupleId(int coupleId) {
+        this.coupleId = coupleId;
+    }
+
+
+    public int getCoupleId() {
+        return coupleId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
