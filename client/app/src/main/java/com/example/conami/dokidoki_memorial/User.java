bb2c77899cd258a62858da33532916b05bfbaf02
kyoumoto <q677224k@mail.kyutech.jp>
2016-11-28 05:18:24 +0900

    [update] User.java

--- client/app/src/main/java/com/example/conami/dokidoki_memorial/User.java
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/User.java
@@ -4,33 +4,36 @@ package com.example.conami.dokidoki_memorial;
  * Created by matsushita on 2016/11/27.
  */
 
-public class User {
-    int id = 1;
-    String name = "user1";
-    int coupleId = 1;
+public final class User {
+    private static int id = 1;
+    private static String name = "user1";
+    private static int coupleId = 1;
 
-    public void setId(int id) {
+    private User(){
+    }
+
+    private void setId(int id) {
         this.id = id;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
-    public void setCoupleId(int coupleId) {
+    private void setCoupleId(int coupleId) {
         this.coupleId = coupleId;
     }
 
 
-    public int getCoupleId() {
+    public static int getCoupleId() {
         return coupleId;
     }
 
-    public int getId() {
+    public static int getId() {
         return id;
     }
 
-    public String getName() {
+    public static String getName() {
         return name;
     }
 }
