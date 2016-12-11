818937cfc36041ce2448553952ae1d4c0cc67fbd
Yuichi Shimo <yuichi@shimomo.net>
2016-12-11 22:30:32 +0900

    [update] refactoring

--- client/app/src/main/java/com/example/conami/dokidoki_memorial/UserModel.java
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/UserModel.java
@@ -1,17 +1,12 @@
 package com.example.conami.dokidoki_memorial;
 
-/**
- * Created by matsushita on 2016/11/27.
- */
-
-public final class User {
+public final class UserModel {
     private static int id = 1;
     private static String name = "user1";
     private static int coupleId = 1;
-    private static int idOfPartnar = 3;
+    private static int partnarId = 3;
 
-    private User(){
-    }
+    private UserModel() {}
 
     private void setId(int id) {
         this.id = id;
@@ -25,10 +20,9 @@ public final class User {
         this.coupleId = coupleId;
     }
 
-    private void setIdOfPartnar(int idOfPartnar){
-        this.idOfPartnar = idOfPartnar;
+    private void setPartnarId(int partnarId) {
+        this.partnarId = partnarId;
     }
-
 
     public static int getId() {
         return id;
@@ -42,7 +36,7 @@ public final class User {
         return coupleId;
     }
 
-    public static int getIdOfPartnar() {
-        return idOfPartnar;
+    public static int getPartnarId() {
+        return partnarId;
     }
 }
