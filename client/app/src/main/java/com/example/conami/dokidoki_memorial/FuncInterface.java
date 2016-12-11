43c71be88bfc72249f8cb712824329e508d5b48a
Yuichi Shimo <yuichi@shimomo.net>
2016-12-11 22:35:18 +0900

    [fix] edit failure

--- client/app/src/main/java/com/example/conami/dokidoki_memorial/FuncInterface.java
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/FuncInterface.java
@@ -1,11 +1,7 @@
-Unstaged changes after reset:
-M	client/app/build.gradle
-M	client/app/src/main/AndroidManifest.xml
-M	client/app/src/main/java/com/example/conami/dokidoki_memorial/FrustrationListFragment.java
-D	client/app/src/main/java/com/example/conami/dokidoki_memorial/Func.java
-M	client/app/src/main/java/com/example/conami/dokidoki_memorial/FuncInterface.java
-M	client/app/src/main/java/com/example/conami/dokidoki_memorial/HttpRequest.java
-M	client/app/src/main/java/com/example/conami/dokidoki_memorial/MainActivity.java
-M	client/app/src/main/java/com/example/conami/dokidoki_memorial/MessageSendingFragment.java
-D	client/app/src/main/java/com/example/conami/dokidoki_memorial/User.java
-M	client/build.gradle
+package com.example.conami.dokidoki_memorial;
+
+import android.widget.TextView;
+
+public interface FuncInterface {
+    public void func(TextView textView, String json);
+}
