b7ff590130f8ef305cd4a0d283fde60fa992c04c
Yuichi Shimo <yuichi@shimomo.net>
2016-12-11 22:32:49 +0900

    [update] refactoring

---
+++ client/app/src/main/java/com/example/conami/dokidoki_memorial/FuncInterface.java
@@ -0,0 +1,11 @@
+Unstaged changes after reset:
+M	client/app/build.gradle
+M	client/app/src/main/AndroidManifest.xml
+M	client/app/src/main/java/com/example/conami/dokidoki_memorial/FrustrationListFragment.java
+D	client/app/src/main/java/com/example/conami/dokidoki_memorial/Func.java
+M	client/app/src/main/java/com/example/conami/dokidoki_memorial/FuncInterface.java
+M	client/app/src/main/java/com/example/conami/dokidoki_memorial/HttpRequest.java
+M	client/app/src/main/java/com/example/conami/dokidoki_memorial/MainActivity.java
+M	client/app/src/main/java/com/example/conami/dokidoki_memorial/MessageSendingFragment.java
+D	client/app/src/main/java/com/example/conami/dokidoki_memorial/User.java
+M	client/build.gradle
