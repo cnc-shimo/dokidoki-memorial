db668b02b5866f1201af04e0efab7023d175096d
Yuichi Shimo <yuichi@shimomo.net>
2016-12-31 23:30:07 +0900

    [add] PushNotificationComponent.php

---
+++ server/cakephp/src/Controller/Component/PushNotificationComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+
+/**
+ * @author shimomo0502
+ */
+class PushNotificationComponent extends Component
+{
+    /**
+     * @param string $title
+     * @param string $body
+     */
+    public function send(string $title, string $body)
+    {
+        // ToDo
+    }
+}
