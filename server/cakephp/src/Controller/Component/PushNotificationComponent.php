de675bf73e50a9ec12d138a5e69eb075fae6784d
Yuichi Shimo <yuichi@shimomo.net>
2017-01-09 01:06:15 +0900

    [update] implement push notification function

--- server/cakephp/src/Controller/Component/PushNotificationComponent.php
+++ server/cakephp/src/Controller/Component/PushNotificationComponent.php
@@ -10,11 +10,37 @@ use Cake\Controller\Component;
 class PushNotificationComponent extends Component
 {
     /**
+     * @var string
+     */
+    private $url = 'https://fcm.googleapis.com/fcm/send';
+
+    /**
      * @param string $title
      * @param string $body
      */
     public function send(string $title, string $body)
     {
-        // ToDo
+        $header = [
+            'Authorization: key=' . getenv('PUSH_NOTIFICATION_KEY'),
+            'Content-Type: application/json',
+        ];
+
+        $data = [
+            'to' => '/topics/news',
+            'priority' => 'high',
+            'notification' => [
+                'title' => $title,
+                'body' => $body,
+            ],
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_exec($ch);
+        curl_close($ch);
     }
 }
